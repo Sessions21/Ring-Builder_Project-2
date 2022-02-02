de445622ac56333dd57dc8c0fa20de2f8d2aef0b
alexvlamakis <alexvlamakis@gmail.com>
2022-02-01 19:46:47 -0700

    login requested with sign up info- user, email, pw, and ph

---
+++ public/javascript/login.js
@@ -0,0 +1,55 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const phone = document.querySelector('#phone-signup').value.trim();
+  
+    if (username && email && password && phone) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+          phone
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
