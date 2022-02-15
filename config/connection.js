467fdaf962cfd1ceba62ce2a0191ed29c244060c
Scott Nichols <s21nichols@hotmail.com>
2022-02-15 14:54:07 -0700

    heroku change

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
-  dialectOptions: {
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-  }
+  port: 3306
 });
 
 module.exports = sequelize;
